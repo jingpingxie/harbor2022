00c78ce786e8989f6b82b65b8227eb3ea1ab43ad
jingpingxie <jason.xie@95isee.com>
2024-10-27 11:28:04 +0800

    dev

--- Assets/Scripts/CraneController.cs
+++ Assets/Scripts/CraneController.cs
@@ -5,32 +5,32 @@ using UnityEngine;
 public class CraneController : MonoBehaviour
 {
 
-    public float moveSpeed = 5f; //移动速度
-    private Transform containerOriginTransform; //集装箱原始位置
-    private GameObject elevator;
-    private GameObject chain;
-    private GameObject hook;
+    public float MoveSpeed = 5f; //移动速度
+    private Transform _containerOriginTransform; //集装箱原始位置
+    private GameObject _elevator;
+    private GameObject _chain;
+    private GameObject _hook;
 
     private CraneActionState moveState = CraneActionState.Unkown;
-    float chainOriginLength;
+    float _chainOriginLength;
     const float CONTAINER_HEIGHT = 4.5f;
 
-    private Transform truckTransform;
+    private Transform _truckTransform;
 
-    GameObject container;
+    GameObject _container;
 
-    GameObject truck;
+    GameObject _truck;
     CarDrive _currentCarDrive;
 
     void Start()
     {
-        elevator = GameObject.Find("Elevator");
-        chain = GameObject.Find("Chain");
-        hook = GameObject.Find("Hook");
-        container = GameObject.Find("Port-Container_SHIP1/Port-container_38");
-        containerOriginTransform = container.transform;
-        truck = GameObject.Find("HG0701");
-        truckTransform = truck.transform;
+        _elevator = GameObject.Find("Elevator");
+        _chain = GameObject.Find("Chain");
+        _hook = GameObject.Find("Hook");
+        _container = GameObject.Find("Port-Container_SHIP1/Port-container_38");
+        _containerOriginTransform = _container.transform;
+        _truck = GameObject.Find("HG0701");
+        _truckTransform = _truck.transform;
 
         System.Console.WriteLine("start");
 
@@ -82,8 +82,8 @@ public class CraneController : MonoBehaviour
 
     private void MoveCrane()
     {
-        Vector3 targetPos = new Vector3(transform.position.x, transform.position.y, containerOriginTransform.position.z + 0.55f);//加上点误差
-        float step = moveSpeed * Time.deltaTime;
+        Vector3 targetPos = new Vector3(transform.position.x, transform.position.y, _containerOriginTransform.position.z + 0.55f);//加上点误差
+        float step = MoveSpeed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, targetPos, step);
         if (Mathf.Abs(transform.position.z - targetPos.z) < 0.01f)
         {
@@ -94,33 +94,33 @@ public class CraneController : MonoBehaviour
 
     private void MoveSlide()
     {
-        Transform elevatorTransform = elevator.transform;
-        Vector3 targetPos = new Vector3(containerOriginTransform.position.x, elevatorTransform.position.y, elevatorTransform.position.z);
-        float step = moveSpeed * Time.deltaTime;
+        Transform elevatorTransform = _elevator.transform;
+        Vector3 targetPos = new Vector3(_containerOriginTransform.position.x, elevatorTransform.position.y, elevatorTransform.position.z);
+        float step = MoveSpeed * Time.deltaTime;
         elevatorTransform.position = Vector3.MoveTowards(elevatorTransform.position, targetPos, step);
         if (Mathf.Abs(elevatorTransform.position.x - targetPos.x) < 0.1f)
         {
             System.Console.WriteLine("move down");
             moveState = CraneActionState.HookDown;
-            Transform chainTransform = chain.transform;
+            Transform chainTransform = _chain.transform;
             Bounds renderBounds = GameObjectUtils.GetCombinedRenderBounds(chainTransform);
-            chainOriginLength = renderBounds.size.y;
+            _chainOriginLength = renderBounds.size.y;
         }
     }
 
     private void HookDown()
     {
-        float moveDistance = moveSpeed * Time.deltaTime / 2;
-        Transform chainTransform = chain.transform;
+        float moveDistance = MoveSpeed * Time.deltaTime / 2;
+        Transform chainTransform = _chain.transform;
 
         Bounds renderBounds = GameObjectUtils.GetCombinedRenderBounds(chainTransform);
-        float scale = (renderBounds.size.y + moveDistance) / chainOriginLength;
+        float scale = (renderBounds.size.y + moveDistance) / _chainOriginLength;
         chainTransform.localScale = new Vector3(chainTransform.localScale.x, chainTransform.localScale.y, scale);
 
-        Transform hookTransform = hook.transform;
+        Transform hookTransform = _hook.transform;
         hookTransform.position = new Vector3(hookTransform.position.x, hookTransform.position.y - moveDistance, hookTransform.position.z);
 
-        float distance = hookTransform.position.y - (containerOriginTransform.position.y + CONTAINER_HEIGHT);
+        float distance = hookTransform.position.y - (_containerOriginTransform.position.y + CONTAINER_HEIGHT);
         if (distance <= 0.0f)
         {
             System.Console.WriteLine("lift up");
@@ -130,17 +130,17 @@ public class CraneController : MonoBehaviour
 
     private void LiftUp()
     {
-        float moveDistance = moveSpeed * Time.deltaTime / 2;
-        Transform chainTransform = chain.transform;
+        float moveDistance = MoveSpeed * Time.deltaTime / 2;
+        Transform chainTransform = _chain.transform;
 
         Bounds renderBounds = GameObjectUtils.GetCombinedRenderBounds(chainTransform);
-        float scale = (renderBounds.size.y - moveDistance) / chainOriginLength;
+        float scale = (renderBounds.size.y - moveDistance) / _chainOriginLength;
         chainTransform.localScale = new Vector3(chainTransform.localScale.x, chainTransform.localScale.y, scale);
 
-        Transform hookTransform = hook.transform;
+        Transform hookTransform = _hook.transform;
         hookTransform.position = new Vector3(hookTransform.position.x, hookTransform.position.y + moveDistance, hookTransform.position.z);
 
-        Transform containerTransform = containerOriginTransform;
+        Transform containerTransform = _containerOriginTransform;
         containerTransform.position = new Vector3(containerTransform.position.x, containerTransform.position.y + moveDistance, containerTransform.position.z);
 
 
@@ -154,16 +154,16 @@ public class CraneController : MonoBehaviour
 
     private void LiftMove()
     {
-        Transform elevatorTransform = elevator.transform;
-        Vector3 targetPos = new Vector3(Mathf.Min(truckTransform.position.x, 8), elevatorTransform.position.y, elevatorTransform.position.z);
-        float step = moveSpeed * Time.deltaTime;
+        Transform elevatorTransform = _elevator.transform;
+        Vector3 targetPos = new Vector3(Mathf.Min(_truckTransform.position.x, 8), elevatorTransform.position.y, elevatorTransform.position.z);
+        float step = MoveSpeed * Time.deltaTime;
 
         elevatorTransform.position = Vector3.MoveTowards(elevatorTransform.position, targetPos, step);
 
-        Transform containerTransform = containerOriginTransform;
+        Transform containerTransform = _containerOriginTransform;
         containerTransform.position = new Vector3(elevatorTransform.position.x, containerTransform.position.y, containerTransform.position.z);
 
-        if (Mathf.Abs(elevatorTransform.position.x - truckTransform.position.x) < 0.1f)
+        if (Mathf.Abs(elevatorTransform.position.x - _truckTransform.position.x) < 0.1f)
         {
             System.Console.WriteLine("lift down");
             moveState = CraneActionState.LiftDown;
@@ -174,41 +174,41 @@ public class CraneController : MonoBehaviour
 
     private void LiftDown()
     {
-        float moveDistance = moveSpeed * Time.deltaTime / 2;
-        Transform chainTransform = chain.transform;
+        float moveDistance = MoveSpeed * Time.deltaTime / 2;
+        Transform chainTransform = _chain.transform;
 
         Bounds renderBounds = GameObjectUtils.GetCombinedRenderBounds(chainTransform);
-        float scale = (renderBounds.size.y + moveDistance) / chainOriginLength;
+        float scale = (renderBounds.size.y + moveDistance) / _chainOriginLength;
         chainTransform.localScale = new Vector3(chainTransform.localScale.x, chainTransform.localScale.y, scale);
 
-        Transform hookTransform = hook.transform;
+        Transform hookTransform = _hook.transform;
         hookTransform.position = new Vector3(hookTransform.position.x, hookTransform.position.y - moveDistance, hookTransform.position.z);
 
-        Transform containerTransform = containerOriginTransform;
+        Transform containerTransform = _containerOriginTransform;
         containerTransform.position = new Vector3(containerTransform.position.x, containerTransform.position.y - moveDistance, containerTransform.position.z);
 
-        float distance = containerOriginTransform.position.y - truckTransform.position.y - 1.4f;
+        float distance = _containerOriginTransform.position.y - _truckTransform.position.y - 1.4f;
         if (distance <= 0.0f)
         {
             System.Console.WriteLine("move back");
             moveState = CraneActionState.MoveBack;
-            container.transform.SetParent(truckTransform);
+            _container.transform.SetParent(_truckTransform);
             if (null != _currentCarDrive) {
-                _currentCarDrive.SetContainerLoaded(container);
+                _currentCarDrive.SetContainerLoaded(_container);
             }
         }
     }
 
     private void MoveBack()
     {
-        float moveDistance = moveSpeed * Time.deltaTime / 2;
-        Transform chainTransform = chain.transform;
+        float moveDistance = MoveSpeed * Time.deltaTime / 2;
+        Transform chainTransform = _chain.transform;
 
         Bounds renderBounds = GameObjectUtils.GetCombinedRenderBounds(chainTransform);
-        float scale = (renderBounds.size.y - moveDistance) / chainOriginLength;
+        float scale = (renderBounds.size.y - moveDistance) / _chainOriginLength;
         chainTransform.localScale = new Vector3(chainTransform.localScale.x, chainTransform.localScale.y, scale);
 
-        Transform hookTransform = hook.transform;
+        Transform hookTransform = _hook.transform;
         hookTransform.position = new Vector3(hookTransform.position.x, hookTransform.position.y + moveDistance, hookTransform.position.z);
 
         if (chainTransform.localScale.z <= 1.0f)
